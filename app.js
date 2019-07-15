0785e8d66308fb7458ead26c3e2a2f44143215c1
Eugene <ewc003@ucsd.edu>
2019-07-14 23:13:50 -0700

    added dummy yargs commands

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const note = getNotes()
+yargs.version('1.1.0')
 
-console.log(note)
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-console.log(validator.isURL('https://mead.o'))
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing out all notes',
+    handler: function () {
+        console.log('Listing notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
+// console.log(yargs.argv)
